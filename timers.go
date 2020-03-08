47d19e5a980891ee93b46754725be7acec29709a
Bill Bai <billbai0102@gmail.com>
2020-03-08 16:46:09 -0400

    tickers in go

--- timers.go
+++ timers.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer1 := time.NewTimer(2 * time.Second)
+
+	<-timer1.C
+	fmt.Println("timer1 started")
+
+	timer2 := time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("timer2 started")
+	}()
+
+	var stop2 bool
+	stop2 = timer2.Stop()
+	if stop2 {
+		fmt.Println("timer2 stopped")
+	}
+
+	time.Sleep(2 * time.Second)
+}
